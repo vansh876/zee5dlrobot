1d5b6aac8c88b12764d9c713e8d51f01e9bb944e
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-03-22 04:18:03 +0530

    Update zee5_dl.py

--- plugins/zee5_dl.py
+++ plugins/zee5_dl.py
@@ -53,29 +53,6 @@ async def zee5_capture(bot, update):
         )
         return
 
-"""@Client.on_message(filters.regex(pattern=".*http.*"))
-async def zee5_capture(bot, update):
-  
-    if update.from_user.id in Config.BANNED_USERS:
-        await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
-        return
-    TRChatBase(update.from_user.id, update.text, "/zee5_capture")
-    update_channel = Config.UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="**Join My Updates Channel to use ME 😎 🤭**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-              ])
-            )
-            return"""
           
     logger.info(update.from_user.id)
     
