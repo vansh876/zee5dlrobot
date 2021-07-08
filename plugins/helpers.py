fcccc2682e0576b366fb957b8a23e98d21d6b7ea
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-08 20:04:34 +0530

    Update helpers.py

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -13,18 +13,12 @@ from script import script
 
 
 
-headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:80.0) Gecko/20100101 Firefox/80.0",
-    "Referer":"https://www.zee5.com",
-    "Accept":"*/*",
-    "Accept-Encoding":"gzip, deflate, br",
-    "Connection":"keep-alive",
-    "Accept-Language":"en-US,en;q=0.9",
-    "Origin":"https://www.zee5.com",
-    "sec-fetch-dest":"empty",
-    "sec-fetch-mode":"cors",
-    "sec-fetch-site":"same-site"
-}
+heads = {
+                "Referer" => "https://www.zee5.com/",
+                "Accept" => "application/json",
+                "Accept-Encoding" => "gzip, deflate, br",
+                "User-Agent" => "Mozilla/5.0 (Linux; Android 10; SM-J400F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36"
+        }
 
 
 async def progress_for_pyrogram(
