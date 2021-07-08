4986bb88c37896f0c76ae65e518ce424fe2646ad
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-08 20:43:06 +0530

    Update zee5_dl.py

--- plugins/zee5_dl.py
+++ plugins/zee5_dl.py
@@ -77,7 +77,7 @@ async def zee5_capture(bot, update):
                     g2 = (r2["hls"][0].replace("drm", "hls"))
                     if "netst" in g2:
                         file_name = r2["title"]
-                        url = g2 + req3["video_token"]               
+                        url = "https://" + li["url"] + g2 + req3["video_token"]               
                     else:
                         file_name = r2["title"]
                         url = "https://" + li["url"] + g2 + req1["video_token"]
@@ -424,7 +424,7 @@ async def zee5_execute(bot, update):
 
                 await bot.edit_message_text(
                     text=script.AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS,
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🙌🏻 SHARE ME 🙌🏻", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40Zee5HEXBot%2A%2A%20%F0%9F%94%A5")]]),
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🙌🏻 SHARE ME 🙌🏻", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40surluploaderbot%2A%2A%20%F0%9F%94%A5")]]),
                     chat_id=update.message.chat.id,
                     message_id=update.message.message_id,
                     disable_web_page_preview=True
